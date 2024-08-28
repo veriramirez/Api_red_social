575adf0619e7f4e1c916938bb55d6f8492d1e0fe
Luis Romano <luisromano@gmail.com>
2024-08-28 16:41:47 -0300

    Controllers y rutas para Oficina

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const PORT = 3000;
 const personaRouter = require("./routes/personaRoutes");
+const oficinaRouter = require("./routes/oficinaRoutes");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/personas", personaRouter);
+app.use("/api/oficinas", oficinaRouter);
 
 app.listen(PORT, () => {
     console.log(`Aplicacion corriendo en puerto ${PORT}`);
---
+++ routes/oficinaRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const oficinaController = require("../controllers/oficinaController");
+
+router.get("/", oficinaController.home);
+router.get("/list", oficinaController.list);
+router.post("/create", oficinaController.create);
+router.get("/info/:id", oficinaController.findById);
+router.put("/update/:id", oficinaController.update);
+router.get("/buscar", oficinaController.buscarPorNombre);
+router.delete("/delete/:id", oficinaController.deleteOficina);
+
+module.exports = router;
