73d756375f0dbabd3a228d86957e0e3a64841a79
Luis Romano <luisromano@gmail.com>
2024-10-30 15:59:56 -0300

    swagger

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const PORT = 3000;
+const swaggerUi = require("swagger-ui-express"); //Para swagger
+const swaggerDocs = require("./swaggerConfig"); // Para swagger
+const path = require("path");
+
 const personaRouter = require("./routes/personaRoutes");
 const oficinaRouter = require("./routes/oficinaRoutes");
 const authRouter = require("./routes/authRoutes");
 const usuarioRouter = require("./routes/usuarioRoutes");
 const followingRouter = require("./routes/followingRoutes");
-const path = require("path");
+
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs)); //Ruta swagger
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use("/api/personas", personaRouter);
