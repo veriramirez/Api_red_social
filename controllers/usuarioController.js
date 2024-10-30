b1cc2b84167fb361d2b5cdabce1968e7a3c33418
Luis Romano <luisromano@gmail.com>
2024-10-30 15:58:21 -0300

    Ruta login a usuario

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,5 +1,7 @@
 const db = require('../models');
 const Usuario = db.Usuario;
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 
 const register = async(req, res) => {
@@ -91,8 +93,39 @@ const list = async(req, res) => {
     }
 }
 
+const login = async(req, res) => {
+    const { mail, password } = req.body;
+
+    try {
+        //1 - Constatar que existe una cuenta con ese mail
+        const usuario = await Usuario.findOne({ where: { mail } });
+        if (!usuario) {
+            return res.status(404).send({ message: "Usuario no encontrada" });
+        }
+        //2 - Verificar password
+        const isMatch = await bcrypt.compare(password, usuario.password);
+        if (!isMatch) {
+            return res.status(400).send({ message: "Password incorrecto" });
+        }
+        //3 - Crear token
+        const token = jwt.sign({
+            id: usuario.id,
+            nombre: usuario.nombre,
+            mail: usuario.mail
+        }, "1234", { expiresIn: 180 });
+        res.status(200).send({ token });
+    } catch (error) {
+        res.status(500).send({
+            message: "Error en el servidor",
+            tipo: error.name,
+            detalles: error.message
+        });
+    }
+}
+
 module.exports = {
     register,
     update,
-    list
+    list,
+    login
 };
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -4,9 +4,147 @@ const usuarioController = require("../controllers/usuarioController");
 const auth = require("../middlewares/authmiddleware");
 const upload = require("../middlewares/uploadMiddleware");
 
-
+/**
+ * @swagger
+ * /usuarios/register:
+ *   post:
+ *     tags: [Usuarios]
+ *     summary: Register a new user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/x-www-form-urlencoded:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nombre:
+ *                 type: string
+ *               mail:
+ *                 type: string
+ *                 format: email
+ *               nickname:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                 nombre:
+ *                   type: string
+ *                 mail:
+ *                   type: string
+ *                 nickname:
+ *                   type: string
+ *       400:
+ *         description: Bad request - missing or invalid fields
+ *       500:
+ *         description: Internal server error
+ */
 router.post("/register", usuarioController.register);
+
+/**
+ * @swagger
+ * /usuarios/me:
+ *   put:
+ *     tags: [Usuarios]
+ *     summary: Update current user's profile
+ *     security:
+ *       - ApiTokenAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nombre:
+ *                 type: string
+ *               nickname:
+ *                 type: string
+ *               mail:
+ *                 type: string
+ *                 format: email
+ *               password:
+ *                 type: string
+ *               avatar:
+ *                 type: string
+ *                 format: binary
+ *     responses:
+ *       200:
+ *         description: User updated successfully
+ *       400:
+ *         description: Bad request - validation error
+ *       404:
+ *         description: User not found
+ */
 router.put("/me", auth, upload.single('avatar'), usuarioController.update);
+
+/**
+ * @swagger
+ * /usuarios/list:
+ *   get:
+ *     tags: [Usuarios]
+ *     summary: List all users with pagination
+ *     parameters:
+ *       - name: page
+ *         in: query
+ *         required: false
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *         description: Page number for pagination
+ *       - name: limit
+ *         in: query
+ *         required: false
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *         description: Number of users per page
+ *     responses:
+ *       200:
+ *         description: A paginated list of users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 totalItems:
+ *                   type: integer
+ *                 totalPages:
+ *                   type: integer
+ *                 currentPage:
+ *                   type: integer
+ *                 itemsPerPage:
+ *                   type: integer
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                       nombre:
+ *                         type: string
+ *                       nickname:
+ *                         type: string
+ *                       mail:
+ *                         type: string
+ *                         format: email
+ *       400:
+ *         description: Bad request - page or limit must be positive
+ *       500:
+ *         description: Internal server error
+ */
 router.get("/list", usuarioController.list);
+
+// Este háganlo uds!!!!!
+router.post("/login", usuarioController.login);
 
 module.exports = router;
