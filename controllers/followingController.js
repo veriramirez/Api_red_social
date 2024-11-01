c272a8ff724773969f06e53dca241480e5ff2d1b
Luis Romano <luisromano@gmail.com>
2024-11-01 20:08:19 -0300

    Controlador following

--- controllers/followingController.js
+++ controllers/followingController.js
@@ -26,6 +26,30 @@ const follow = async (req, res) => {
         
 };
 
+// Obtener la lista de usuarios que el usuario sigue
+const getFollowing = async(req, res) => {
+    const id_usuario = req.user.id;
+
+    try {
+        const usuario = await db.Usuario.findByPk(id_usuario, {
+            include: [{
+                model: db.Usuario,
+                as: 'seguidos', // Usa la relación "seguidos"
+                attributes: ['id', 'nombre', 'nickname'],
+            }, ],
+        });
+
+        if (!usuario) {
+            return res.status(404).send({ error: 'Usuario no encontrado' });
+        }
+
+        res.status(200).send(usuario.seguidos); // Enviar solo los usuarios seguidos
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+};
+
+
 
 
 module.exports = {
