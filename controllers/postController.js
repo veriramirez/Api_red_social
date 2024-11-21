578673bb52e346d2e4b8d00a106386f530deb4ea
Vero Ramirez <veronica.ramirez@itscordoba.edu.ar>
2024-11-21 11:22:04 -0300

    creo la funcion listgetpost - documentada en swagger

--- controllers/postController.js
+++ controllers/postController.js
@@ -22,8 +22,33 @@ const newPost = async (req, res) => {
         res.status(500).send({ message: "Error al crear el post" });
         console.error(error);
     }
-};              
+};
+
+
+
+//que muestre todos los posts
+//si no tiene que informe
+//si sale otro error que sea de servidor
+const listgetposts = async(req, res) => {
+    try {
+        const listaPost = await Post.findAll();
+
+        if (listaPost.length > 0) {
+            res.status(200).send(listaPost);
+        } else {
+            res.status(404).send({ error: 'No hay post' });
+        }
+    } catch (error) {
+        res.status(500).send({message: "error de servidor"});
+    }
+};
+
+
+
+
+
 
 module.exports = {
-    newPost
+    newPost,
+    listgetposts,
 };
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -55,4 +55,44 @@ const upload = require("../middlewares/uploadMiddleware");
 
 router.post("/", auth, postController.newPost);
 
+
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Post:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *         id_usuario:
+ *           type: integer
+ *         titulo:
+ *           type: string
+ *         contenido:
+ *           type: string
+ *         createdAt:
+ *           type: string
+ *           format: date-time
+ *
+ * /posts:
+ *   get:
+ *     tags: [Post]
+ *     summary: Lista todas las publicaciones
+ *     responses:
+ *       200:
+ *         description: Muestra lista de posts
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Post'
+ *       404:
+ *         description: No hay posts
+ *       500:
+ *         description: Error interno del servidor
+ */
+
+router.get("/", auth, postController.listgetposts);
+
 module.exports = router;
